1be2d0557250caa957417f67c96be3ac47367251
coyz2 <coyz2@lnx-coyz2-1.lunet.lboro.ac.uk>
2023-01-03 14:56:31 +0000

    142

---
+++ Yixiao/142.linked-list-cycle-ii.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=142 lang=python
+#
+# [142] Linked List Cycle II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if(head == None):
+            return None
+            
+        slow = head
+        fast = head
+
+        while True:
+            if(fast.next == None or fast.next.next == None):
+                return None
+            fast = fast.next.next
+            slow = slow.next
+            if(fast == slow):
+                break
+        
+        fast = head
+        while(fast != slow):
+            fast = fast.next
+            slow = slow.next
+        return fast
+
+
+
+
+        
+# @lc code=end
+
