7481fbe5b0ab10011987c5ab680987466b97a609
coyz2 <coyz2@lnx-coyz2-1.lunet.lboro.ac.uk>
2023-01-25 11:24:46 +0000

    70

---
+++ Yixiao/70.climbing-stairs.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=70 lang=python
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if(n==1):
+            return 1
+        if(n==2):
+            return 2
+        dp = [0 for i in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2,n):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[-1]
+
+
+        
+# @lc code=end
+
